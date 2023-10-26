a6f0f8d02ce4e1e84f90332f1a90ba0b8ed7d0e6
OlyaaFed <115480389+OlyaaFed@users.noreply.github.com>
2023-10-26 20:54:32 +0300

    Add files via upload

---
+++ 22/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите колличество чисел");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int b=Convert.ToInt32(Console.ReadLine());
+int c= Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int count = 0;
+for (int i = 0; i < a; i++)
+{
+    if (b % 10 == c)
+    {
+        sum += i;
+        count++;
+    }
+
+}
+if (count > 3)
+{
+    Console.WriteLine($"Сумма  чисел, которые оканчиваются на {c} равна {sum}");
+}
+else
+{
+    Console.WriteLine($"Недостаточно чисел, которые оканчиваются на {c}, чтобы найти сумму");
+}
---
+++ 25/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите значение первого прямоугольного параллелепипеда");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение первого прямоугольного параллелепипеда");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение первого прямоугольного параллелепипеда");
+double c = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение второго прямоугольного параллелепипеда");
+double d = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение второго прямоугольного параллелепипеда");
+double e = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение второго прямоугольного параллелепипеда");
+double f = Convert.ToDouble(Console.ReadLine());
+
+double x = 0;
+double y = 0;
+
+x = a * b * c;
+y = d * e * f;
+if (x > y)
+
+    Console.WriteLine("Первый прямоугольный параллелепипед будет больше");
+
+else
+    Console.WriteLine("Второй прямоугольный параллелепипед будет больше");
